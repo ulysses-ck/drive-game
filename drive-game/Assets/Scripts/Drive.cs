ba91ad37aac38e59ef63d0b20d11dcaed10dd948
Ulises <ulisesapaza.280@gmail.com>
2025-12-06 20:56:02 -0300

    modify script to steer and modify tire example

--- drive-game/Assets/Scripts/Drive.cs
+++ drive-game/Assets/Scripts/Drive.cs
@@ -1,37 +1,57 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Drive : MonoBehaviour
 {
-    [SerializeField] private WheelCollider wheelCollider;
-
+    [SerializeField] private WheelCollider[] WCs;
+    [SerializeField] public GameObject[] wheels;
     public float torque = 200f;
+    public float maxSteerAngle = 30;
 
     private float currentAccelerationInput = 0f;
+    private float currentSteer = 0f;
 
     void Start()
     {
-        if (wheelCollider == null)
-        {
-            wheelCollider = GetComponent<WheelCollider>();
-        }
+
     }
 
     private void FixedUpdate()
     {
-        Go(currentAccelerationInput);
+        Go(currentAccelerationInput, currentSteer);
     }
 
-    void Go(float accel)
+    void Go(float accel, float steer)
     {
         accel = Mathf.Clamp(accel, -1f, 1f);
+        steer = Mathf.Clamp(steer, -1f, 1f) * maxSteerAngle;
 
         float thrustTorque = accel * torque;
-        wheelCollider.motorTorque = thrustTorque;
+        for(int i = 0; i < wheels.Length; i++)
+        {
+            WCs[i].motorTorque = thrustTorque;
+
+            if (i<2)
+                WCs[i].steerAngle = steer;
+
+            Quaternion quat;
+            Vector3 position;
+
+            WCs[i].GetWorldPose(out position, out quat);
+
+            wheels[i].transform.position = position;
+            wheels[i].transform.rotation = quat;
+        }
     }
 
     public void OnAccelerate(InputAction.CallbackContext context)
     {
         currentAccelerationInput = context.ReadValue<float>();
+    }
+
+    public void OnSteer(InputAction.CallbackContext context)
+    {
+        currentSteer = context.ReadValue<float>();
     }
 }
